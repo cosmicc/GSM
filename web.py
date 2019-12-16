a603928e7db9f695cf5353a7f7a2cac042c77100
unknown <ianperry99@gmail.com>
2019-12-15 20:07:24 -0500

    update

--- web.py
+++ web.py
@@ -124,7 +124,7 @@ def index():
         lavg = int(sum(lp) / len(lp))
         tavg = float_trunc_1dec(sum(t) / len(t))
         havg = float_trunc_1dec(sum(h) / len(h))
-        if livedata[1] is not None:
+        if livedata is not None:
             b = 0 + (100 - 0) * ((livedata[1] - 100000) / (300 - 100000))
             if int(b) < 0:
                 light2 = 0
