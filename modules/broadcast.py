81652a439b25d85bd5f610ffba937fac0996a7eb
unknown <ianperry99@gmail.com>
2019-12-16 23:58:28 -0500

    update

--- modules/broadcast.py
+++ modules/broadcast.py
@@ -2,11 +2,13 @@ import socket
 from loguru import logger as log
 from time import sleep
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
-client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-client.bind(("", 37020))
-while True:
-    data, addr = client.recvfrom(1024)
-    log.info("received broadcast: %s" % data)
-    sleep(.1)
+
+def bcast():
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    client.bind(("", 37020))
+    while True:
+        data, addr = client.recvfrom(1024)
+        log.info("received broadcast: %s" % data)
+        sleep(.1)
